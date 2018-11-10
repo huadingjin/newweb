768fb1af352055ed95a08466fe20063ce6121708
huadingjin <huadingjin@163.com>
2018-11-11 00:10:18 +0800

    first page

---
+++ webserver/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func firstPage(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Hello, this is my first web page!<h1>")
+}
+
+func main() {
+	http.HandleFunc("/", firstPage)
+	http.ListenAndServe(":8000", nil)
+	
+}
